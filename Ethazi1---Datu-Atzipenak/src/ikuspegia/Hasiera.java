ef8ca2b78305f2238a2c710ed7d832fcf75d12d0
ErLoriz <erlantz.lorizab@elorrieta-errekamari.com>
2019-10-29 10:04:23 +0100

    Hasiera

--- Ethazi1---Datu-Atzipenak/src/ikuspegia/Hasiera.java
+++ Ethazi1---Datu-Atzipenak/src/ikuspegia/Hasiera.java
@@ -24,7 +24,7 @@ public class Hasiera extends JFrame {
 	 */
 	public Hasiera() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 630, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
